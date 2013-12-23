3af64e58f7719dca0a177ad03c6ed0893feb0b4f
Murtaza Husain <murtaza52@gmail.com>
2013-12-23 15:07:31 +0530

    added processors

--- src/tweet_summary/twitter_api.clj
+++
@@ -1,6 +0,0 @@
-(ns tweet-summary.twitter-api
-  (:require [twitter.api.restful :refer [statuses-user-timeline]]))
-
-(defn fetch-tweets
-  [creds user tweet-count]
-  (statuses-user-timeline :oauth-creds creds :screen-name user :count tweet-count :include_rts false))
